6bd9efe95a0bae6da7de3ebb9dc11048969745f2
amr818 <amr818@ic.ac.uk>
2020-05-28 19:08:23 +0300

    Implement Insert Prepared SQL Statement and add PostGIS JDBC jar to dependencies

--- database/src/QueryProcessor.java
+++ database/src/QueryProcessor.java
@@ -1,13 +1,14 @@
+import org.postgis.PGgeometry;
+
 import java.io.*;
+import java.net.MalformedURLException;
 import java.sql.*;
-import java.util.Properties;
+import java.util.*;
 
 public class QueryProcessor {
 
   private static final String CONFIG_FILEPATH = "database/src/config.properties";
 
-  private File configFile;
-  private FileReader fileReader;
   private Properties properties;
   private Connection connection;
 
@@ -15,10 +16,10 @@ public class QueryProcessor {
     /* Do not connect by default upon creation */
     connection = null;
 
-    configFile = new File(CONFIG_FILEPATH);
+    File configFile = new File(CONFIG_FILEPATH);
     properties = new Properties();
 
-    fileReader = new FileReader(configFile);
+    FileReader fileReader = new FileReader(configFile);
     properties.load(fileReader);
 
     String driver = properties.getProperty("driver");
@@ -60,6 +61,74 @@ public class QueryProcessor {
         e.printStackTrace();
       }
     }
+  }
+
+
+/** Prepared SQL Statements methods
+ *
+  */
+
+  public int insertInto (String tableName, String... values) throws SQLException, MalformedURLException {
+    PreparedStatement stmt = null;
+    tableName = tableName.toUpperCase();
+    List<String> args = Arrays.asList(values);
+    int index = 0;
+    boolean tableExists = true;
+
+    switch (tableName) {
+      case "PLAYER": {
+          stmt = connection.prepareStatement("insert into PLAYER values(?, ?, ?)");
+          stmt.setInt(1, Integer.parseInt(args.get(0)));
+          stmt.setString(2, args.get(1));
+          stmt.setString(3, args.get(2));
+          break;
+        }
+      case "PLAYER_STATS": {
+        stmt = connection.prepareStatement("insert into PLAYER_STATS values(?, ?, ?)");
+        stmt.setInt(1, Integer.parseInt(args.get(0)));
+        stmt.setInt(2, Integer.parseInt(args.get(1)));
+        stmt.setInt(3, Integer.parseInt(args.get(2)));
+        break;
+      }
+      case "GALLERY": {
+        stmt = connection.prepareStatement("insert into GALLERY values(?, ?, ?, ?)");
+        stmt.setInt(1, Integer.parseInt(args.get(0)));
+        stmt.setTimestamp(2, Timestamp.valueOf(args.get(1)));
+        stmt.setInt(3, Integer.parseInt(args.get(2)));
+        stmt.setString(4, args.get(3));
+        break;
+      }
+      case "LOCATION": {
+        stmt = connection.prepareStatement("insert into LOCATION values(?, ?)");
+        stmt.setInt(1, Integer.parseInt(args.get(0)));
+        stmt.setObject(2, PGgeometry.geomFromString(args.get(1)));
+        break;
+      }
+      case "LANDMARK": {
+        stmt = connection.prepareStatement("insert into LANDMARK values(?, ?, ?, ?)");
+        stmt.setInt(1, Integer.parseInt(args.get(0)));
+        stmt.setObject(2, PGgeometry.geomFromString(args.get(1)));
+        stmt.setObject(3, Integer.parseInt(args.get(2)));
+        stmt.setString(4, args.get(3));
+        break;
+      }
+      case "LANDMARK_TYPE": {
+        stmt = connection.prepareStatement("insert into LANDMARK_TYPE values(?, ?)");
+        stmt.setInt(1, Integer.parseInt(args.get(0)));
+        stmt.setString(2, args.get(1));
+        break;
+      }
+      default: {
+        System.out.println("Table " + tableName + " does not exist");
+        tableExists = false;
+      }
+    }
+
+    if(tableExists) {
+      index = stmt.executeUpdate();
+    }
+
+    return index;
   }
 
 
