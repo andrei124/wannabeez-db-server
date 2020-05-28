b1350fa3e95f36224c780985e9a4203755b718db
amr818 <amr818@ic.ac.uk>
2020-05-28 15:46:36 +0300

    Create connection to database server

--- database/src/QueryProcessor.java
+++ database/src/QueryProcessor.java
@@ -4,20 +4,25 @@ import java.util.Properties;
 
 public class QueryProcessor {
 
+  private static final String CONFIG_FILEPATH = "database/src/config.properties";
+
   private File configFile;
   private FileReader fileReader;
   private Properties properties;
+  private Connection connection;
 
   public QueryProcessor() throws ClassNotFoundException, IOException {
-    configFile = new File("database/src/config.properties");
+    /* Do not connect by default upon creation */
+    connection = null;
+
+    configFile = new File(CONFIG_FILEPATH);
     properties = new Properties();
 
     fileReader = new FileReader(configFile);
     properties.load(fileReader);
 
-     String driver = properties.getProperty("driver");
-     registerDriver(driver);
-
+    String driver = properties.getProperty("driver");
+    registerDriver(driver);
   }
 
   private void registerDriver(String driver) throws ClassNotFoundException {
@@ -31,21 +36,31 @@ public class QueryProcessor {
     }
   }
 
-  public void connect() {
-    Connection connection = null;
-
+  public Connection connect() {
     String url = properties.getProperty("url");
     String user = properties.getProperty("user");
     String password = properties.getProperty("password");
 
-
-    /** TODO: create connection to Database
     try {
-        connection = DriverManager.getConnection(url, user, password);
-        System.out.println("Connection to PostgreSQL server established successfully\n");
+      connection = DriverManager.getConnection(url, user, password);
+      System.out.println("Connection to PostgreSQL server established successfully\n");
     } catch (SQLException e) {
-        System.out.println("SQL Exception: " + e.getMessage());
-    } */
-
+      System.out.println("SQL Exception: " + e.getMessage());
+    }
+    return connection;
   }
+
+  public void closeConnection() {
+    if(connection != null) {
+      try {
+        connection.close();
+        System.out.println("Connection succesfully closed");
+      } catch (SQLException e) {
+        System.out.println("Error occured when closing connection");
+        e.printStackTrace();
+      }
+    }
+  }
+
+
 }
