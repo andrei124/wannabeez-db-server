00b0a74da0f968cef1b0a5c40a287af8a176306e
amr818 <amr818@ic.ac.uk>
2020-05-28 15:05:17 +0300

    Create config file for database connection

--- database/src/QueryProcessor.java
+++ database/src/QueryProcessor.java
@@ -1,21 +1,49 @@
+import java.io.*;
 import java.sql.*;
-
+import java.util.Properties;
 
 public class QueryProcessor {
 
-    public QueryProcessor() {
-        try {
-            registerDriver();
-        } catch (ClassNotFoundException e) {
-            System.out.println("JDBC Driver could not be registered");
-            System.exit(1);
-            e.printStackTrace();
-        }
-    }
+  private File configFile;
+  private FileReader fileReader;
+  private Properties properties;
 
-    private void registerDriver() throws ClassNotFoundException {
-        Class.forName("org.postgresql.Driver");
-    }
+  public QueryProcessor() throws ClassNotFoundException, IOException {
+    configFile = new File("database/src/config.properties");
+    properties = new Properties();
 
+    fileReader = new FileReader(configFile);
+    properties.load(fileReader);
+
+    /** TODO: Register the JDBC Driver
+     String driver = properties.getProperty("driver");
+     registerDriver(driver);
+     */
+  }
+
+  private void registerDriver(String driver) throws ClassNotFoundException {
+    try {
+      Class.forName(driver);
+    } catch (ClassNotFoundException e) {
+      System.out.println("JDBC Driver could not be registered");
+      System.exit(1);
+      e.printStackTrace();
+    }
+  }
+
+  public void connect() {
+    Connection connection = null;
+    String url = properties.getProperty("url");
+    String user = properties.getProperty("user");
+    String password = properties.getProperty("password");
+
+
+    /** TODO: create connection to Database
+     *  try {
+        connection = DriverManager.getConnection(url, user, password);
+    } catch (SQLException e) {
+        System.out.println("SQL Exception: " + e.getMessage());
+    } */
+
+  }
 }
-
