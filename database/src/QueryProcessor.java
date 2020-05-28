16c9b3c776fc3c1a24724cc3ef58f185344faa3a
amr818 <amr818@ic.ac.uk>
2020-05-28 15:19:28 +0300

    Register PostgreSQL JDBC driver from config file

--- database/src/QueryProcessor.java
+++ database/src/QueryProcessor.java
@@ -15,15 +15,15 @@ public class QueryProcessor {
     fileReader = new FileReader(configFile);
     properties.load(fileReader);
 
-    /** TODO: Register the JDBC Driver
      String driver = properties.getProperty("driver");
      registerDriver(driver);
-     */
+
   }
 
   private void registerDriver(String driver) throws ClassNotFoundException {
     try {
       Class.forName(driver);
+      System.out.println("Database driver loaded succesfully");
     } catch (ClassNotFoundException e) {
       System.out.println("JDBC Driver could not be registered");
       System.exit(1);
@@ -33,14 +33,16 @@ public class QueryProcessor {
 
   public void connect() {
     Connection connection = null;
+
     String url = properties.getProperty("url");
     String user = properties.getProperty("user");
     String password = properties.getProperty("password");
 
 
     /** TODO: create connection to Database
-     *  try {
+    try {
         connection = DriverManager.getConnection(url, user, password);
+        System.out.println("Connection to PostgreSQL server established successfully\n");
     } catch (SQLException e) {
         System.out.println("SQL Exception: " + e.getMessage());
     } */
